039d4ae95dcb2c46be3673d8e41e13f9f487f807
Hani Jahan <haniahn1115@gmail.com>
2021-12-28 11:48:29 +0330

    shortestRemainingTimeFirst algorithm added

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ q1 = 0
 q2 = 0
 processes = []
 
+
 class Process:
-    def __init__(self, execTime, startTime):
-        self.execTime = execTime
-        self.startTime = startTime
+    def __init__(self, label, execTime, startTime):
+        self.label = label
+        self.execTime = int(execTime)
+        self.startTime = int(startTime)
+        self.runningTime = 0
 
 
 def loadFile(fileName):
     with open(fileName, 'r') as file:
         processCount = int(file.readline())
+        labels = list(map(lambda i: f'P{i}', range(processCount)))
 
         execTimes = file.readline().split(',')
-        execTimes = map(execTimes, lambda x: x.strip())
+        execTimes = list(map(lambda x: x.strip(), execTimes))
 
         startTimes = file.readline().split(',')
-        startTimes = map(startTimes, lambda x: x.strip())
+        startTimes = list(map(lambda x: x.strip(), startTimes))
 
         global q1
         q1 = int(file.readline())
@@ -24,6 +28,42 @@ def loadFile(fileName):
         q2 = int(file.readline())
 
         global processes
-        processes = map(zip(execTimes, startTimes),
-                        lambda x: Process(x[0], x[1]))
+        processes = list(map(lambda x: Process(x[0], x[1], x[2]), zip(
+            labels, execTimes, startTimes)))
+        processes.sort(key=lambda process: process.startTime)
 
+
+def srtf():
+    running = None
+    global processes
+    processesCopy = processes.copy()
+    readyQueue = []
+    i = 0
+    while len(processesCopy) != 0 or len(readyQueue) != 0:
+        for process in processesCopy:
+            if process.startTime == i:
+                readyQueue.append(process)
+                processesCopy.remove(process)
+            elif process.startTime > i:
+                break
+        srt = None
+        for process in readyQueue:
+            if not srt or srt.execTime > process.execTime:
+                srt = process
+        if srt and srt is running:
+            srt.runningTime += 1
+        elif srt and running:
+            print(f'{running.label}({running.runningTime})', end='--')
+            running.runningTime = 0
+            running = srt
+            running.runningTime += 1
+        elif srt:
+            running = srt
+            running.runningTime += 1
+        if srt: 
+            srt.execTime -= 1
+        i += 1
+        if srt and srt.execTime == 0: readyQueue.remove(srt)
+    print(f'{running.label}({running.runningTime})', end='')
+loadFile('test.txt')
+srtf()
