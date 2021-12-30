28f9585180006b946d0dd87922d28974a31a5ed5
Hani Jahan <haniahn1115@gmail.com>
2021-12-30 17:29:40 +0330

    Highest Response Ratio Next algorithm added

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ processes = []  # All processes
 
 
 class Process:
-    def __init__(self, label, execTime, startTime):
+    def __init__(self, label, burstTime, arivalTime):
         self.label = label
-        self.execTime = int(execTime)
-        self.remainingTime = self.execTime
-        self.startTime = int(startTime)
-        self.endTime = -1
-        self.runningTime = 0
+        self.burstTime = int(burstTime)
+        self.arivalTime = int(arivalTime)
+        self.turnaroundTime = 0
+        self.exitTime = -1
+        self.remainingTime = self.burstTime
+        self.responceRatio = 0
 
     # copies a process
     def copy(self):
-        return Process(self.label, self.execTime, self.startTime)
+        return Process(self.label, self.burstTime, self.arivalTime)
 
-    # changes remaining time for a process
+    # runs process for 1 time (decreases remaining time by 1)
     def run(self):
         self.remainingTime -= 1
 
@@ -26,11 +27,15 @@ class Process:
 
     # calculates turnaround time
     def turnAroundTime(self):
-        return self.endTime - self.startTime
+        return self.exitTime - self.arivalTime
+
+    # calculates turnaround time by waiting time
+    def turnAroundTime_byWT(self):
+        return self.burstTime + self.waitingTime()
 
     # calculates waiting time
     def waitingTime(self):
-        return self.turnAroundTime() - self.execTime
+        return self.turnAroundTime() - self.burstTime
 
 
 def loadFile(fileName):
@@ -40,13 +45,13 @@ def loadFile(fileName):
         # sets a label for every process
         labels = list(map(lambda i: f'P{i}', range(processCount)))
 
-        # creates a list containing execution time of each process
-        execTimes = file.readline().split(',')
-        execTimes = list(map(lambda x: x.strip(), execTimes))
+        # creates a list containing burst time of each process
+        burstTimes = file.readline().split(',')
+        burstTimes = list(map(lambda x: x.strip(), burstTimes))
 
-        # creates a list containing start time of each process
-        startTimes = file.readline().split(',')
-        startTimes = list(map(lambda x: x.strip(), startTimes))
+        # creates a list containing arival time of each process
+        arivalTimes = file.readline().split(',')
+        arivalTimes = list(map(lambda x: x.strip(), arivalTimes))
 
         # sets Quantum 1
         global q1
@@ -58,9 +63,9 @@ def loadFile(fileName):
         global processes
         # creates a list of tupples containig all data of a process
         processes = list(map(lambda x: Process(
-            x[0], x[1], x[2]), zip(labels, execTimes, startTimes)))
-        # sorts all processes by shortest start time
-        processes.sort(key=lambda process: process.startTime)
+            x[0], x[1], x[2]), zip(labels, burstTimes, arivalTimes)))
+        # sorts all processes by arival time
+        processes.sort(key=lambda process: process.arivalTime)
 
 
 def srtf():
@@ -90,41 +95,41 @@ def srtf():
     while len(processesCopy) != 0 or len(readyQueue) != 0:
         # for all the processes:
         for process in processesCopy:
-            # if the start time of a process equals to the time we're on,
-            if process.startTime == time:
+            # if the arival time of a process equals to the time we're on,
+            if process.arivalTime == time:
                 # adds the process to ready queue,
                 readyQueue.append(process)
                 # and removes it from the processes list.
                 processesCopy.remove(process)
 
-            # when reaches the first start time that
+            # when reaches the first arival time that
             #  we haven't reached yet, gets out of for loop
-            elif process.startTime > time:
+            elif process.arivalTime > time:
                 break
         srt = None
         # srt is the process with shortest remaining time
 
         for process in readyQueue:
             # if there is no srt, or the process has less
-            # execution time than srt: set the process as srt
-            if not srt or srt.execTime > process.execTime:
+            # burst time than srt: set the process as srt
+            if not srt or srt.burstTime > process.burstTime:
                 srt = process
 
-        # if there is srt and it's also running, increase running time by 1
+        # if there is srt and it's also running, increase turnaround time by 1
         if srt and srt is running:
-            srt.runningTime += 1
+            srt.turnaroundTime += 1
 
         # if srt and running are different, print the running process and replace them
         elif srt and running:
-            print(f'{running.label}({running.runningTime})', end=' --> ')
-            running.runningTime = 0
+            print(f'{running.label}({running.turnaroundTime})', end=' --> ')
+            running.turnaroundTime = 0
             running = srt
-            running.runningTime += 1
+            running.turnaroundTime += 1
 
         # when nothing is running, set srt as running
         elif srt:
             running = srt
-            running.runningTime += 1
+            running.turnaroundTime += 1
 
         # each time loop starts, decrease remaining time of running by 1
         if running:
@@ -133,16 +138,16 @@ def srtf():
         time += 1
 
         # when running is finished,
-        # set the endTime to the time
+        # set the exitTime to the time
         # and remove it from ready queue
         if running and running.isFinished():
-            running.endTime = time
+            running.exitTime = time
             readyQueue.remove(running)
             # calculate sum of turnaround time and sum of waiting time
             sumTT += running.turnAroundTime()
             sumWT += running.waitingTime()
     # print the running process
-    print(f'{running.label}({running.runningTime})')
+    print(f'{running.label}({running.turnaroundTime})')
     # calculate sum of turnaround time and sum of waiting time
     sumTT += running.turnAroundTime()
     sumWT += running.waitingTime()
@@ -169,7 +174,7 @@ def hrrn():
     print()
     # response ratio = 1 + w/s
     # where w is total waiting time until the time
-    # and s is service time of process (execTime)
+    # and s is service time of process (burstTime)
 
     running = None
     # 'runing' is the process that is running at the time
@@ -184,8 +189,51 @@ def hrrn():
     sumTT = 0
     # set sum of waiting time
     sumWT = 0
-    #########################################################
-    #########################################################
+
+    while len(processesCopy) != 0 or len(readyQueue) != 0:
+        for process in processesCopy:
+            if process.arivalTime == time:
+
+                readyQueue.append(process)
+                processesCopy.remove(process)
+
+            elif process.arivalTime > time:
+                break
+
+        hrr = None
+        for process in readyQueue:
+            process.responseRatio = 1 + (
+                (process.burstTime + (time - process.arivalTime)) / process.burstTime)
+            if not hrr or hrr.responseRatio < process.responseRatio:
+                hrr = process
+        if hrr and hrr is running:
+            hrr.turnaroundTime += 1
+        elif hrr and running:
+            print(f'{running.label}({running.turnaroundTime})', end=' --> ')
+            running.turnaroundTime = 0
+            running = hrr
+            running.turnaroundTime += 1
+
+        elif hrr:
+            running = hrr
+            running.turnaroundTime += 1
+
+        if running:
+            running.run()
+
+        time += 1
+
+        if running and running.isFinished():
+            running.exitTime = time
+            readyQueue.remove(running)
+            sumTT += running.turnAroundTime()
+            sumWT += running.waitingTime()
+
+    print(f'{running.label}({running.turnaroundTime})')
+
+    sumTT += running.turnAroundTime()
+    sumWT += running.waitingTime()
+
     # calculate average of turnaround time and average of waiting time
     avgTT = sumTT / len(processes)
     avgWT = sumWT / len(processes)
